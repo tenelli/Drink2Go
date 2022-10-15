a3b97c9883c3b8923e6d81f98ca4cd267e5437ec
Николь <n.kozerenko@yandex.ru>
2022-10-16 00:06:57 +0300

    Полная черновая версия

--- source/js/script.js
+++ source/js/script.js
@@ -23,3 +23,36 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+
+const map = L.map('map')
+  .setView({
+    lat: 59.968435975884354,
+    lng: 30.317528294049186,
+  }, 20);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../../img/main_pin.svg',
+    iconSize: [38, 50]
+  });
+
+  const marker = L.marker(
+    {
+      lat: 59.968435975884354,
+      lng: 30.317528294049186,
+    },
+
+    {
+      icon: mainPinIcon,
+    }
+  )
+  .bindPopup("Drink2Go - Интернет магазин кофейных напитков")
+  .addTo(map);;
+
